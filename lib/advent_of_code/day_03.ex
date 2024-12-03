c43ebcbc38c82f9c553e838382bffa01c385b4c4
zuzia-kru <zuzia.kru@gmail.com>
2024-12-03 23:11:59 +0100

    Day 3 part 1

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,5 +1,16 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(args) do
+    ~r/mul\(\d{1,3},\d{1,3}\)/
+    |> Regex.scan(args)
+    |> Enum.map(&do_multiply/1)
+    |> Enum.sum()
+  end
+
+  defp do_multiply([command]) do
+    ~r/\d{1,3}/
+    |> Regex.scan(command)
+    |> Enum.map(fn [x] -> String.to_integer(x) end)
+    |> Enum.product()
   end
 
   def part2(_args) do
