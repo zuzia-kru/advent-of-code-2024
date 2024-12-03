e5a42c104b408d460b816530649c7dbf84df711d
zuzia-kru <zuzia.kru@gmail.com>
2024-12-03 22:40:51 +0100

    Day 2 Part 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -23,6 +23,29 @@ defmodule AdventOfCode.Day02 do
     end
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> String.split("\n")
+    |> Enum.reject(fn l -> l == "" end)
+    |> Enum.map(fn line -> String.split(line) end)
+    |> Enum.map(fn chars -> Enum.map(chars, &String.to_integer/1) end)
+    |> Enum.map(fn report -> relaxed_safe?(report) end)
+    |> Enum.sum()
+  end
+
+  defp relaxed_safe?([head | tail] = report) do
+    case safe?(report) do
+      1 -> 1
+      0 -> safe_removing_level?([], head, tail)
+    end
+  end
+
+  defp safe_removing_level?(start, _, []), do: safe?(start)
+
+  defp safe_removing_level?(start, elem, [finhead | fintail] = finish) do
+    case safe?(start ++ finish) do
+      1 -> 1
+      0 -> safe_removing_level?(start ++ [elem], finhead, fintail)
+    end
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -15,11 +15,15 @@ defmodule AdventOfCode.Day02Test do
     assert result == 2
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
     result = part2(input)
 
-    assert result
+    assert result == 4
   end
 end
